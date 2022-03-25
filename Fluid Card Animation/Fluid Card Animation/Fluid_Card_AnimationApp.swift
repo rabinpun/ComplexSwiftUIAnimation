3f5b2151553ee8fceaebec7a44f93a97cafcf2f7
rabin.pun <rabin.pun@ebpearls.com>
2022-03-25 12:49:04 +0545

    inital commit

---
+++ Fluid Card Animation/Fluid Card Animation/Fluid_Card_AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fluid_Card_AnimationApp.swift
+//  Fluid Card Animation
+//
+//  Created by ebpearls on 25/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct Fluid_Card_AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
